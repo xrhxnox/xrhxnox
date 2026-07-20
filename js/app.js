e9174d2c386025396248de29a6ec57554de21fa8
RicardoHdzm <jrhm95@gmail.com>
2026-07-20 02:24:55 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -172,7 +172,7 @@ const THEME_KEY = "ricstars_theme";
 
 function updateThemeIcon() {
   const theme = document.documentElement.getAttribute("data-theme");
-  themeToggle.innerHTML = theme === "light" ? '<i class="fa-solid fa-bolt"></i>' : '<i class="fa-solid fa-moon"></i>';
+  themeToggle.innerHTML = theme === "light" ? '<i class="fa-solid fa-moon"></i>' : '<i class="fa-solid fa-bolt"></i>';
 }
 updateThemeIcon();
 
