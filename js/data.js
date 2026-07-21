3e82c555d290f1384069eceaf954814685270e83
RicardoHdzm <jrhm95@gmail.com>
2026-07-20 18:33:52 -0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -231,6 +231,6 @@ const entries = [
     generos: ["Gadgets"],
     resena: "La sucesora de, en mi opinión, la mejor consola de Nintendo. La mejora en hardware y software se nota muchísimo. Principalmente la uso en modo portátil; siendo sinceros, la batería podría durar un poco más, aunque nunca me ha representado un gran problema. No puedo esperar a ver todos los juegos que llegarán en los próximos años. Si Nintendo mantiene el nivel de sus first party, tiene todo para convertirse en la mejor consola de la historia.",
     puntuacion: 5,
-    fecha: "2025-02-25"
+    fecha: "2025-06-25"
   }
 ];
