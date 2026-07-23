be00cf95bfb8f57ecaafcad27451737a70e03aa9
RicardoHdzm <jrhm95@gmail.com>
2026-07-22 20:50:54 -0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -338,7 +338,7 @@ const entries = [
     categoria: "pelicula",
     imagen: "assets/img/masters-of-the-universe-2026-35.jpg",
     generos: ["Acción","Aventura","Ciencia ficción","Fantasía"],
-    resena: "La historia está muy sencilla, pero la verdad me la pasé muy bien. Nunca intenta tomarse demasiado en serio y eso le ayuda bastante. La acción y las peleas son lo mejor de la película; siento que están muy bien llevadas. A mitad de la película sí se cae un poco el ritmo, pero al final creo que logra remontar. Si solo quieres apagar el cerebro un rato y entretenerte, cumple de sobra.",
+    resena: "La historia es muy básica y simple, pero la verdad me la pasé con madre lol. La pelicula sabe que no debe tomarse demasiado en serio y justo por eso logra que la acción y el humor sean lo mejor de la película. Siento que a mitad de la película sí se cae un poco el ritmo, pero al final creo que logra remontar. Es de esas peliculas en donde lo mejor es apagar el cerebro y disfrutar.",
     puntuacion: 7.5,
     fecha: "2026-07-22"
   }
